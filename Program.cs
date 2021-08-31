5468c9101937a86341f8ed2f56c7fa055864d200
Michael C. Tiberio <MichaelTiberio@users.noreply.github.com>
2021-08-31 15:27:25 -0700

    Adding new .net project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankocr0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
