4e333e548c06b41d3d944edc58993c343e9e5c7b
Michael C. Tiberio <MichaelCTiberio@users.noreply.github.com>
2021-09-28 14:18:55 -0700

    Refactoring build scripts

--- scripts/RunAcceptance.cmd
+++ scripts/RunAcceptance.cmd
@@ -8,18 +8,14 @@ echo ^>
 echo ^> ---------- ACCEPTANCE STAGE RUN ----------
 
 REM Set the build root directory
-    echo ^>
-    echo ^> cd %BUILD_ROOT%
-    cd %BUILD_ROOT%
+    call %SCRIPT_PATH%Task.cmd cd %BUILD_ROOT%
     if errorlevel 1 exit /b 1
 
     REM The mess below will echo the output of 'cd' with some decoration.
     for /f %%i in ('cd') do echo ^> Build root directory: %%i
 
 REM Run the Acceptance tests
-    echo ^>
-    echo ^> call %SCRIPT_PATH%AcceptanceTests.cmd
-    call %SCRIPT_PATH%AcceptanceTests.cmd
+    call %SCRIPT_PATH%Task.cmd %SCRIPT_PATH%AcceptanceTests.cmd
     if errorlevel 1 exit /b 1
     echo ^>
 
--- scripts/RunCommit.cmd
+++ scripts/RunCommit.cmd
@@ -8,37 +8,27 @@ echo ^>
 echo ^> ---------- COMMIT STAGE RUN ----------
 
 REM Set the build root directory
-    echo ^>
-    echo ^> cd %BUILD_ROOT%
-    cd %BUILD_ROOT%
+    call %SCRIPT_PATH%Task.cmd cd %BUILD_ROOT%
     if errorlevel 1 exit /b 1
 
     REM The mess below will echo the output of 'cd' with some decoration.
     for /f %%i in ('cd') do echo ^> Build root directory: %%i
 
 REM Call the build script
-    echo ^>
-    echo ^> call %SCRIPT_PATH%Build.cmd
-    call %SCRIPT_PATH%Build.cmd
+    call %SCRIPT_PATH%Task.cmd %SCRIPT_PATH%Build.cmd
     if errorlevel 1 exit /b 1
 
 REM Verify the test categories
-    echo ^>
-    echo ^> call %SCRIPT_PATH%VerifyTestCategories.cmd
-    call %SCRIPT_PATH%VerifyTestCategories.cmd
+    call %SCRIPT_PATH%Task.cmd %SCRIPT_PATH%VerifyTestCategories.cmd
     if errorlevel 1 exit /b 1
 
 REM Run the commit tests
-    echo ^>
-    echo ^> call %SCRIPT_PATH%CommitTests.cmd
-    call %SCRIPT_PATH%CommitTests.cmd
+    call %SCRIPT_PATH%Task.cmd %SCRIPT_PATH%CommitTests.cmd
     if errorlevel 1 exit /b 1
     echo ^>
 
 REM Publish the binaries
-    echo ^>
-    echo ^> call %SCRIPT_PATH%Publish.cmd
-    call %SCRIPT_PATH%Publish.cmd
+    call %SCRIPT_PATH%Task.cmd  %SCRIPT_PATH%Publish.cmd
     if errorlevel 1 exit /b 1
     echo ^>
 
